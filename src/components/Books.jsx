ac45e6a2fff2a3865f4e8391454bd202de9eb027
SMNoushan <noushan.md@gmail.com>
2024-03-27 00:30:37 +0600

    added footer

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -7,7 +7,7 @@ const Books = ({ books, styles = "" }) => {
             <h1 className={`text-center text-[40px] font-bold font-playfair_display mb-12 ${styles}`}>This is Books</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 *:mx-auto gap-6 *:w-[320px] md:*:min-w-[362px]">
                 {
-                    books.slice(0, 6).map(book => <BookCard key={book.id} book={book} />)
+                    books.slice(0, 6).map(book => <BookCard key={book.bookId} book={book} />)
                 }
             </div>
             <div className="text-center mt-12">
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const MainLayout = () => {
     return (
         <div className="max-w-[425px] md:container xl:max-w-[1170px] mx-auto text-dark-01 py-2.5">
             <Navbar />
             <Outlet />
+            <Footer />
         </div>
     );
 };
