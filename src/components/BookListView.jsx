5d5010b4fe7c91044027fca27843bb096c71d050
SMNoushan <noushan.md@gmail.com>
2024-03-27 08:21:39 +0600

    updated read books(responsive styles)

--- src/components/BookListView.jsx
+++ src/components/BookListView.jsx
@@ -19,16 +19,16 @@ const BookListView = ({ book }) => {
         totalPages
     } = book;
     return (
-        <div className="flex border border-dark-01/15 p-6 rounded-2xl gap-6">
-            <div className="rounded-2xl bg-dark-01/5 size-60 flex justify-center items-center">
-                <img className="w-32 h-44" src={`.${image}`} alt="book-cover" />
+        <div className="flex flex-col md:flex-row border border-dark-01/15 p-6 rounded-2xl gap-6">
+            <div className="rounded-2xl mx-auto bg-dark-01/5 size-80 md:size-60 flex justify-center items-center">
+                <img className="w-52 md:w-32 h-72 md:h-44" src={`.${image}`} alt="book-cover" />
             </div>
-            <div className="flex-1 space-y-4">
-                <h1 className="font-bold text-2xl font-playfair_display">{bookName}</h1>
-                <h3 className="opacity-80 font-medium">By: {author}</h3>
-                <div className="flex gap-6 items-center">
+            <div className="flex-1 space-y-2 lg:space-y-4">
+                <h1 className="font-bold text-2xl font-playfair_display text-center lg:text-start">{bookName}</h1>
+                <h3 className="opacity-80 font-medium text-center lg:text-start">By: {author}</h3>
+                <div className="flex flex-col lg:flex-row gap-1 lg:gap-6 items-center">
                     <p className="font-bold">Tag</p>
-                    <div className="flex gap-1.5 items-center *:px-2 *:py-2 *:bg-green-01/5 *:text-green-01 *:font-medium *:rounded-[30px] mx-2">
+                    <div className="flex gap-x-1.5 items-center flex-wrap *:px-2 *:py-2 *:bg-green-01/5 *:text-green-01 *:text-sm md:*:text-base *:font-medium *:rounded-[30px] mx-2">
                         {
                             tags.map((tag, idx) => <p key={idx}>{`#${tag}`}</p>)
                         }
@@ -38,7 +38,7 @@ const BookListView = ({ book }) => {
                         <p>Year of Publishing: {yearOfPublishing}</p>
                     </div>
                 </div>
-                <div className="flex gap-6 items-center opacity-60">
+                <div className="flex flex-col lg:flex-row gap-1 lg:gap-6 items-center opacity-60">
                     <div className="flex gap-2 items-center">
                         <HiOutlineUsers />
                         <p>Publisher: {publisher}</p>
@@ -49,15 +49,15 @@ const BookListView = ({ book }) => {
                     </div>
                 </div>
                 <div className="border border-dashed border-dark-01/15"></div>
-                <div className="grid grid-cols-12 gap-6">
-                    <div className="col-span-4">
-                        <Button label={`Category: ${category}`} styles="rounded-full w-full bg-blue-02/15 hover:bg-blue-02/30 text-blue-02 md:text-base md:font-normal" />
+                <div className="grid grid-cols-3 md:grid-cols-12 gap-6">
+                    <div className="col-span-2 md:col-span-5 lg:col-span-4">
+                        <Button label={`Category: ${category}`} styles="btn-sm rounded-full w-full bg-blue-02/15 hover:bg-blue-02/30 text-blue-02 md:text-sm lg:text-base md:font-normal" />
                     </div>
-                    <div className="col-span-3">
-                        <Button label={`Rating: ${rating}`} styles="rounded-full w-full bg-orange-01/15 hover:bg-orange-01/30 text-orange-01 md:text-base md:font-normal" />
+                    <div className="md:col-span-4 lg:col-span-3">
+                        <Button label={`Rating: ${rating}`} styles="btn-sm rounded-full w-full bg-orange-01/15 hover:bg-orange-01/30 text-orange-01 md:text-sm lg:text-base md:font-normal" />
                     </div>
-                    <div className="col-span-3">
-                        <Button url={`/book/${bookId}`} label='View Details' styles="rounded-full bg-green-01 hover:bg-green-01/80 text-white md:text-base" />
+                    <div className="col-span-3 mx-auto md:col-span-3">
+                        <Button url={`/book/${bookId}`} label='View Details' styles="btn-sm rounded-full bg-green-01 hover:bg-green-01/80 text-white md:text-sm lg:text-base" />
                     </div>
                 </div>
             </div>
